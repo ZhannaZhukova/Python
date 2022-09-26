b858838887f576c4d4b6ac53ce543510eabc46e0
Zhanna <zhuzha.jukova@yandex.ru>
2022-09-26 17:42:02 +0700

    Add new solution for task5

--- Task5.py
+++ Task5.py
@@ -1,41 +1,11 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
-x = 0
-y = 0
-z = 0
-check = not (x or y or z) == (not x and not y and not z)
-print('Проверка истинности условия ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z с данными значениями X = x, Y = y, Z = z -> check')
-x = 0
-y = 0
-z = 1
-check = not (x or y or z) == (not x and not y and not z)
-print('Проверка истинности условия ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z с данными значениями X = x, Y = y, Z = z -> check')
-x = 0
-y = 1
-z = 0
-check = not (x or y or z) == (not x and not y and not z)
-print('Проверка истинности условия ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z с данными значениями X = x, Y = y, Z = z -> check')
-x = 0
-y = 1
-z = 1
-check = not (x or y or z) == (not x and not y and not z)
-print('Проверка истинности условия ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z с данными значениями X = x, Y = y, Z = z -> check')
-x = 1
-y = 0
-z = 0
-check = not (x or y or z) == (not x and not y and not z)
-print('Проверка истинности условия ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z с данными значениями X = x, Y = y, Z = z -> check')
-x = 1
-y = 0
-z = 1
-check = not (x or y or z) == (not x and not y and not z)
-print('Проверка истинности условия ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z с данными значениями X = x, Y = y, Z = z -> check')
-x = 1
-y = 1
-z = 0
-check = not (x or y or z) == (not x and not y and not z)
-print('Проверка истинности условия ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z с данными значениями X = x, Y = y, Z = z -> check')
-x = 1
-y = 1
-z = 1
-check = not (x or y or z) == (not x and not y and not z)
-print('Проверка истинности условия ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z с данными значениями X = x, Y = y, Z = z -> check')
+def isTrue(x, y, z):
+    if not (x or y or z) == (not x) and (not y) and (not z):
+        return 'ИСТИННО'
+    return 'ЛОЖНО'
+
+print('Проверка истинности утверждения ¬(X v Y v Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(f'При X = {x}, Y = {y}, Z = {z}) - {isTrue(x, y, z)}')
